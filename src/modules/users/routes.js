c8e3243414af1a85fabafd9682e7f32b4e762c46
roni-iq <ironi@iquantile.com>
2022-01-17 15:07:32 +0600

    base router and child routing component organized

---
+++ src/modules/users/routes.js
@@ -0,0 +1,16 @@
+import Home from "@/modules/users/views/Home";
+
+export const userRoutes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../../modules/users/views/About.vue')
+    }]
